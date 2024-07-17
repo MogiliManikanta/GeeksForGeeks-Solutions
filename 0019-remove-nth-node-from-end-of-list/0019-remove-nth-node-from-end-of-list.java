6028f44c80f826a97d78e8f2685689cd0ce6221c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-07-17 12:23:51 +0530

    Time: 0 ms (100%), Space: 41.7 MB (34.17%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int N) {
+      // Create two pointers, fastp and slowp
+        ListNode fastp = head;
+        ListNode slowp = head;
+
+        // Move the fastp pointer N nodes ahead
+        for (int i = 0; i < N; i++)
+            fastp = fastp.next;
+
+        // If fastp becomes null, the Nth node from the end is the head
+        if (fastp == null)
+            return head.next;
+
+        // Move both pointers until fastp reaches the end
+        while (fastp.next != null) {
+            fastp = fastp.next;
+            slowp = slowp.next;
+        }
+
+        // Delete the Nth node from the end
+        ListNode delNode = slowp.next;
+        slowp.next = slowp.next.next;
+        delNode = null;
+        return head;
+    }
+}
+/***A one pass solution can be done using pointers. Move one pointer fast --> n+1 places forward, to maintain a gap of n between the two pointers and then move both at the same speed. Finally, when the fast pointer reaches the end, the slow pointer will be n+1 places behind - just the right spot for it to be able to skip the next node.
+
+Since the question gives that n is valid, not too many checks have to be put in place. Otherwise, this would be necessary.
+ */ 
