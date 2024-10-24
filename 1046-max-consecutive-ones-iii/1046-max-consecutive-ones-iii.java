3a0cef66cef13fbd8db0d7a0b7a274ca142f8a46
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-24 22:32:18 +0530

    Time: 4 ms (28.84%), Space: 47.5 MB (11.18%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,20 +1,59 @@
 class Solution {
-    public int longestOnes(int[] A, int K) {
-        int start = 0, max = Integer.MIN_VALUE, zeroCounter = 0;
-        for (int end = 0; end < A.length; end++) {
-            if (A[end] == 0) {
-                zeroCounter++;
+    public int longestOnes(int[] nums, int k) {
+        int left=0,right=0,zeroes=0,maxi=0;
+        while(right<nums.length){
+            if(nums[right]==0) zeroes++;
+            if(zeroes>k){
+                if(nums[left]==0) zeroes--;
+                left++;
             }
-            while (zeroCounter > K) {
-                if (A[start] == 0) {
-                    zeroCounter--;
-                }
-                start++;
-            }
-
-            max = Math.max(max, end - start + 1);
+            if(zeroes<=k) maxi=Math.max(maxi,right-left+1);
+            right++;
         }
-
-        return max;
+        return maxi;
     }
 }
+/*class Solution {
+    public int longestOnes(int[] nums, int k) {
+        // if(k==0) return 0;
+        int left=0,right=0,maxi=0,zeroes=0;
+        while(right<nums.length){
+            if(nums[right]==0){
+                zeroes++;
+            }
+            if(zeroes>k){
+                while(left<right && zeroes>k){
+                    if(nums[left]==0){
+                        zeroes--;
+                    }
+                    left++;
+                }
+            }
+            if(zeroes<=k){
+            maxi=Math.max(maxi,right-left+1);
+            }
+            right++;
+        }
+        return maxi;
+    }
+}*/
+/**class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int maxi=0;
+        for(int i=0;i<nums.length;i++){
+            int zeroes=0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]==0){
+                    zeroes++;
+                }
+                if(zeroes<=k){
+                    maxi=Math.max(maxi,j-i+1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return maxi;
+    }
+} */
