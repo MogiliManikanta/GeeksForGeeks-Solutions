f11c4a053e3e3e1b81e9d10f51b06a7e660b04b7
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2024-10-25 19:53:00 +0530

    Time: 2 ms (86.24%), Space: 45.6 MB (89.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int swapBallsSize=0;
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i]==0) swapBallsSize++;
+        int swapBalls=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                swapBalls++;
+            }
             else{
-                if(swapBallsSize>0) { 
-                    int temp=nums[i];
-                    nums[i]=nums[i-swapBallsSize];
-                    nums[i-swapBallsSize] = temp;
-                }
+                int temp = nums[i];
+                nums[i]=nums[i-swapBalls];
+                nums[i-swapBalls]=temp;
             }
         }
     }
